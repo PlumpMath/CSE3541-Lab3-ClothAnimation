1d8b6a289b72ba50e18ff03f0df8602762aa13b7
Kurt Mueller <kurtronaldmueller@gmail.com>
2016-03-02 10:32:34 -0500

    Add ClothMotion namespace

---
+++ Scripts/ClothMotion.fs
@@ -0,0 +1,18 @@
+﻿namespace ClothMotion
+open UnityEngine
+
+type Edge = 
+    { Vertex1: int;
+      Vertex2: int; }
+
+type ClothEdges =
+    { Edges: Edge list }
+
+type ClothMotion() =
+    inherit MonoBehaviour()
+
+    let t = 0.075f
+    let mass = 1.0f
+    let damping = 0.99f
+    let stiffness = 1000.0f
+
