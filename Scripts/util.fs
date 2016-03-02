9724da7cd031968acbd00803e124a0c70eef7227
Kurt Mueller <kurtronaldmueller@gmail.com>
2016-03-02 10:30:58 -0500

    Add Util module and slicesBySize method.

---
+++ Scripts/util.fs
@@ -0,0 +1,11 @@
+﻿module Util
+
+let rec slicesBySize size list =
+    match list with
+    | [] -> [] // case needed for type inference
+    | list when list.Length < size -> [list]
+    | _ ->
+        let first = list |> Seq.take size |> List.ofSeq
+        let rest = list |> Seq.skip size |> List.ofSeq
+
+        [first] @ slicesBySize size rest
